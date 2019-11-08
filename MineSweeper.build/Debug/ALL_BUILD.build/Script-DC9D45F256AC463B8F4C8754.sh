5473d83bcd935d7dafe05b6645c8b4151eb20e9f
Chad Hurst <chadhurst4@gmail.com>
2019-11-07 22:31:33 -0700

    Removing ignored files

--- MineSweeper.build/Debug/ALL_BUILD.build/Script-DC9D45F256AC463B8F4C8754.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/chadhurst/repo/ChadBentley -f /Users/chadhurst/repo/ChadBentley/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
